a8c998390582df7ba5c5ace2309a641fa25f2bb5
tania-n <137098@epsstudents.org>
2024-09-04 20:55:46 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+public class Main{
+	public static void main(String[ ] args){
+	    System.out.println(/*"Tania Bhatti, 9/3/2024, Unit 0 Initials Project. Using our initials, my partner and I created a star and a larger version of our initials."*/);
+	    //I used the N to create the outline of the start
+		System.out.println("        N");
+		System.out.println("       N  N"); 
+		System.out.println("      N    N"); 
+		System.out.println("NNNNNN      NNNNNN"); 
+		//I used the T to create eyes for the star 
+		//The B is used to create a mouth for the star
+		System.out.println(" N    T    T    N");
+		System.out.println("  N     B      N"); 
+		System.out.println("   N    N    N"); 
+		System.out.println("  N  N      N  N");
+		System.out.println( "N               N"); 
+		System.out.println(""); 
+		//This is my partners part of the project
+		System.out.println("RRRRRRR  CCCCCCCC  CCCCCCCC    \nRR  RR   CCC       CCC\nRRRRRR   CCC       CCC\nRR   RR  CCC       CCC \nRR   RR  CCCCCCCC  CCCCCCCC\n");
+	}
+}
